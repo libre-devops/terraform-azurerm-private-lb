29544a5d5219e0ebbeca36cbe3af31e0ed2698b4
Craig Thacker <craigthackerx@gmail.com>
2022-04-23 14:42:32 +0100

    update lb

--- output.tf
+++ output.tf
@@ -1,26 +1,26 @@
 output "lb_id" {
-  value       = azurerm_lb.pub_lb.id
+  value       = azurerm_lb.priv_lb.id
   description = "The ID of the load balancer"
 }
 
 output "lb_name" {
-  value       = azurerm_lb.pub_lb.name
+  value       = azurerm_lb.priv_lb.name
   description = "The Name of the load balancer"
 }
 
 output "lb_ip_configuration" {
-  value       = azurerm_lb.pub_lb.frontend_ip_configuration
+  value       = azurerm_lb.priv_lb.frontend_ip_configuration
   description = "The frontend ip configuration object"
 }
 
 output "bpool_id" {
-  value       = azurerm_lb_backend_address_pool.public_lb_bpool.id
+  value       = azurerm_lb_backend_address_pool.private_lb_bpool.id
   description = "The id of the backend pool"
 
 }
 
 output "bpool_name" {
-  value       = azurerm_lb_backend_address_pool.public_lb_bpool.id
+  value       = azurerm_lb_backend_address_pool.private_lb_bpool.id
   description = "The name of the backend pool"
 }
 
