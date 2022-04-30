235e15ec05d4997d3152913292a08ed6a32c9f9a
Craig Thacker <craigthackerx@gmail.com>
2022-05-01 00:52:50 +0100

    Update module

--- input.tf
+++ input.tf
@@ -1,19 +1,5 @@
-variable "rg_name" {
-  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
-  type        = string
-  validation {
-    condition     = length(var.rg_name) > 1 && length(var.rg_name) <= 24
-    error_message = "Resource group name is not valid."
-  }
-}
-
-variable "location" {
-  description = "The location for this resource to be put in"
-  type        = string
-}
-
-variable "lb_name" {
-  description = "The name of the LB"
+variable "lb_bpool_name" {
+  description = "The name for the backend pool for the Load Balancer"
   type        = string
 }
 
@@ -23,15 +9,29 @@ variable "lb_frontend_ip_configurations" {
   default     = {}
 }
 
+variable "lb_name" {
+  description = "The name of the LB"
+  type        = string
+}
+
 variable "lb_sku_name" {
   description = "The SKU of the lb"
   type        = string
   default     = "Standard"
 }
 
-variable "lb_bpool_name" {
-  description = "The name for the backend pool for the Load Balancer"
+variable "location" {
+  description = "The location for this resource to be put in"
   type        = string
+}
+
+variable "rg_name" {
+  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
+  type        = string
+  validation {
+    condition     = length(var.rg_name) > 1 && length(var.rg_name) <= 24
+    error_message = "Resource group name is not valid."
+  }
 }
 
 variable "tags" {
--- output.tf
+++ output.tf
@@ -1,18 +1,3 @@
-output "lb_id" {
-  value       = azurerm_lb.priv_lb.id
-  description = "The ID of the load balancer"
-}
-
-output "lb_name" {
-  value       = azurerm_lb.priv_lb.name
-  description = "The Name of the load balancer"
-}
-
-output "lb_ip_configuration" {
-  value       = azurerm_lb.priv_lb.frontend_ip_configuration
-  description = "The frontend ip configuration object"
-}
-
 output "bpool_id" {
   value       = azurerm_lb_backend_address_pool.private_lb_bpool.id
   description = "The id of the backend pool"
@@ -22,4 +7,19 @@ output "bpool_id" {
 output "bpool_name" {
   value       = azurerm_lb_backend_address_pool.private_lb_bpool.id
   description = "The name of the backend pool"
+}
+
+output "lb_id" {
+  value       = azurerm_lb.priv_lb.id
+  description = "The ID of the load balancer"
+}
+
+output "lb_ip_configuration" {
+  value       = azurerm_lb.priv_lb.frontend_ip_configuration
+  description = "The frontend ip configuration object"
+}
+
+output "lb_name" {
+  value       = azurerm_lb.priv_lb.name
+  description = "The Name of the load balancer"
 }
